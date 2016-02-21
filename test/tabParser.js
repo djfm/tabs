24082d070c21ff218c48ff255cc9e31787323f66
djfm <fm.de.jouvencel@gmail.com>
2016-02-21 16:59:03 +0100

    tabParser splits the tab into sections

---
+++ test/tabParser.js
@@ -0,0 +1,29 @@
+import chai from 'chai';
+chai.should();
+
+import {extractSections} from '../lib/tabParser';
+
+/* global describe, it */
+describe("The tab parser", function () {
+    it("should extract sections from a tab", function () {
+        extractSections(`
+            Title: Hello World
+            some content
+            Author: Somebody
+            Intro:
+            This is
+
+            the intro
+            :stuffz
+            Chorus:
+            [A] la la la la la
+
+        `).should.deep.equal([
+            {type: "definition", name: "Title", title: "Hello World", body: "some content"},
+            {type: "definition", name: "Author", title: "Somebody", body: ""},
+            {type: "definition", name: "Intro", title: "", body: "This is\n\nthe intro"},
+            {type: "reference", name: "stuffz", title: "", body: ""},
+            {type: "definition", name: "Chorus", title: "", body: "[A] la la la la la"},
+        ]);
+    });
+});
